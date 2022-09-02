e2805ed65ef50d14f879d6014e73121601cc62ea
kChoiyg <dudrb1418@gmail.com>
2022-09-02 17:11:43 +0900

    Time: 759 ms (88.31%), Space: 18.3 MB (81.82%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.swift
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.swift
@@ -1,15 +1,32 @@
 class Solution {
+// func findDuplicate(_ nums: [Int]) -> Int {
+//     var dict = [Int: Int]()
+    
+//     nums.forEach {
+//         dict[$0, default: 0] += 1
+//     }
+    
+//     let result = dict.filter {
+//         $0.value > 1
+//     }.first
+// //    print(result!.key)
+//     return result!.key
+// }
+    
 func findDuplicate(_ nums: [Int]) -> Int {
-    var dict = [Int: Int]()
-    
-    nums.forEach {
-        dict[$0, default: 0] += 1
+guard nums.isEmpty == false else {
+    return 0
+}
+
+var backup = [Int:Bool]()
+
+for item in nums {
+    if backup[item] == true {
+        return item
     }
-    
-    let result = dict.filter {
-        $0.value > 1
-    }.first
-//    print(result!.key)
-    return result!.key
+    backup[item] = true
+}
+
+return nums[0]
 }
 }
