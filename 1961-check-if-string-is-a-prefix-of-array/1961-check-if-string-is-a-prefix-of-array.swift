0487f5682047b37c633a8491c4f25e823d15f893
kChoiyg <dudrb1418@gmail.com>
2022-09-06 08:52:07 +0900

    Time: 22 ms (27.27%), Space: 15 MB (9.09%) - LeetHub

--- 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.swift
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.swift
@@ -1,16 +1,48 @@
 class Solution {
-func isPrefixString(_ s: String, _ words: [String]) -> Bool {
-    var prev = words[0]
-    for i in 1..<words.count {
-        if !(s.prefix(prev.count) == prev) {
+// func isPrefixString(_ s: String, _ words: [String]) -> Bool {
+//     var prev = words[0]
+//     for i in 1..<words.count {
+//         if !(s.prefix(prev.count) == prev) {
+//             return false
+//         }
+//         else if s.count == prev.count {
+//             return true
+//         }
+//         prev += words[i]
+//     }
+    
+//     return (s == prev)
+// }
+    
+//     func isPrefixString(_ s: String, _ words: [String]) -> Bool {
+//         var string = ""
+
+//         for word in words {
+
+//             if !s.contains(string) {
+//                 return false
+//             }
+
+//             if s == string {
+//                 return true
+//             }
+//         }
+
+//         return false
+//     }
+    func isPrefixString(_ s: String, _ words: [String]) -> Bool {
+    var string = ""
+
+    for word in words {
+        string += word
+        guard s.contains(string) else {
             return false
         }
-        else if s.count == prev.count {
+        guard s != string else {
             return true
         }
-        prev += words[i]
     }
     
-    return (s == prev)
+    return false
 }
 }
