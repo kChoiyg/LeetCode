9d05a77031b2d602b27bc2df5754bc8a92fd8953
kChoiyg <dudrb1418@gmail.com>
2022-09-15 17:15:21 +0900

    Time: 399 ms (20.00%), Space: 15.5 MB (90.00%) - LeetHub

--- 524-longest-word-in-dictionary-through-deleting/524-longest-word-in-dictionary-through-deleting.swift
+++ 524-longest-word-in-dictionary-through-deleting/524-longest-word-in-dictionary-through-deleting.swift
@@ -1,37 +1,73 @@
 class Solution {
-func findLongestWord(_ s: String, _ dictionary: [String]) -> String {
-    var result = [String]()
-    var maxValue = Int.min
+// func findLongestWord(_ s: String, _ dictionary: [String]) -> String {
+//     var result = [String]()
+//     var maxValue = Int.min
 
-    let dictionary = dictionary.sorted(by: { (first, second) in
-        first.count > second.count
-    })
+//     let dictionary = dictionary.sorted(by: { (first, second) in
+//         first.count > second.count
+//     })
+    
+//     for word in dictionary {
+//         let arrStr = Array(s)
+//         let arrWord = Array(word)
+//         var j = 0
+//         var temp = ""
+        
+//         for i in 0..<arrStr.count {
+//             if j == arrWord.count {
+//                 break
+//             }
+            
+//             if arrStr[i] == arrWord[j] {
+//                 temp += String(arrWord[j])
+//                 j += 1
+//             }
+//         }
+        
+//         if temp == word && maxValue <= word.count {
+//             result.append(word)
+//             maxValue = max(maxValue, word.count)
+//         }
+//     }
+    
+//     result = result.sorted(by: <)
+    
+//     return result.count == 0 ? "":result[0]
+// }
+    
+    
+    func findLongestWord(_ s: String, _ dictionary: [String]) -> String {
+    var result = ""
     
     for word in dictionary {
         let arrStr = Array(s)
         let arrWord = Array(word)
         var j = 0
         var temp = ""
-        
+
         for i in 0..<arrStr.count {
             if j == arrWord.count {
                 break
             }
-            
+
             if arrStr[i] == arrWord[j] {
                 temp += String(arrWord[j])
                 j += 1
             }
         }
+     
+        if temp != word {
+            continue
+        }
         
-        if temp == word && maxValue <= word.count {
-            result.append(word)
-            maxValue = max(maxValue, word.count)
+        if word.count > result.count {
+            result = word
+        }
+        else if temp.count == result.count {
+            result =  min(result, temp)
         }
     }
-    
-    result = result.sorted(by: <)
-    
-    return result.count == 0 ? "":result[0]
+
+    return result
 }
 }
