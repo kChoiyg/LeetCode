1580ec88cf30f6f6328e5480e949d26cb4569a07
kChoiyg <dudrb1418@gmail.com>
2022-09-22 16:44:54 +0900

    Time: 2305 ms (28.00%), Space: 21.6 MB (66.00%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.swift
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.swift
@@ -33,25 +33,52 @@ class Solution {
 // }
     
     
-private func collectGarbage(_ garbage: [String], _ travel: [Int], type: Character) -> Int {
-    var time = 0
-    var res = 0
+// private func collectGarbage(_ garbage: [String], _ travel: [Int], type: Character) -> Int {
+//     var time = 0
+//     var res = 0
+
+//     for i in 0..<garbage.count {
+//         let n = garbage[i].filter { $0 == type }.count
+
+//         if n > 0 {
+//             time += n
+//             res = time
+//         }
+//         time += travel[i]
+//     }
+
+//     return res
+// }
+// func garbageCollection(_ garbage: [String], _ travel: [Int]) -> Int {
+//    return "MGP"
+//         .map { collectGarbage(garbage, travel + [0], type: $0) }
+//         .reduce(0, +)
+// }
+    
+    
+    func garbageCollection(_ garbage: [String], _ travel: [Int]) -> Int {
+    var mapGarbageType = [Character: Int]()
+    var mapTravelTime = [Character: Int]()
+    let travel = travel + [0]
+    var travelTime = 0
 
     for i in 0..<garbage.count {
-        let n = garbage[i].filter { $0 == type }.count
-
-        if n > 0 {
-            time += n
-            res = time
+        for j in 0..<garbage[i].count {
+            let garbageType = Array(garbage[i]).sorted(by: <)[j]
+            mapGarbageType[garbageType, default: 0] += 1
+            mapTravelTime[garbageType, default: 0] = travelTime
         }
-        time += travel[i]
+        
+        travelTime += travel[i]
     }
 
-    return res
-}
-func garbageCollection(_ garbage: [String], _ travel: [Int]) -> Int {
-   return "MGP"
-        .map { collectGarbage(garbage, travel + [0], type: $0) }
-        .reduce(0, +)
+    return mapGarbageType.reduce(0) { result, data -> Int in
+        var newResult = result + data.value
+        if let time = mapTravelTime[data.key] {
+            newResult += time
+        }
+
+        return newResult
+    }
 }
 }
