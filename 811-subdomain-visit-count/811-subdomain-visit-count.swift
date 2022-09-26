db2b40787f168391cc0ebf6698bc0be881f781d9
kChoiyg <dudrb1418@gmail.com>
2022-09-26 16:55:06 +0900

    Time: 108 ms (50.00%), Space: 15.5 MB (13.16%) - LeetHub

--- 811-subdomain-visit-count/811-subdomain-visit-count.swift
+++ 811-subdomain-visit-count/811-subdomain-visit-count.swift
@@ -1,23 +1,49 @@
 class Solution {
+// func subdomainVisits(_ cpdomains: [String]) -> [String] {
+//     var dict = [String: Int]()
+    
+//     for cpdomain in cpdomains {
+//         let split = cpdomain.components(separatedBy: " ")
+//         guard let count = split.first,
+//               let domains = split.last?.components(separatedBy: ".")
+//         else {
+//             continue
+//         }
+        
+//         var prev = ""
+//         for i in (0..<domains.count).reversed() {
+//             prev = "." + domains[i] + prev
+//             let newDomain = prev.suffix(prev.count - 1)
+//             dict[String(newDomain), default: 0] += Int(count) ?? 0
+//         }
+//     }
+    
+//     return dict.compactMap { "\($0.value) \($0.key)" }
+// }
+    
+    
 func subdomainVisits(_ cpdomains: [String]) -> [String] {
     var dict = [String: Int]()
-    
+
     for cpdomain in cpdomains {
+
         let split = cpdomain.components(separatedBy: " ")
         guard let count = split.first,
-              let domains = split.last?.components(separatedBy: ".")
+              let domain = split.last
         else {
             continue
         }
-        
-        var prev = ""
-        for i in (0..<domains.count).reversed() {
-            prev = "." + domains[i] + prev
-            let newDomain = prev.suffix(prev.count - 1)
-            dict[String(newDomain), default: 0] += Int(count) ?? 0
+
+        var arrDomain = Array(domain)
+        var dotIndex = arrDomain.firstIndex(of: ".") ?? 0
+
+        while dotIndex > 0 {
+            dict[String(arrDomain), default: 0] += Int(count) ?? 0
+            dotIndex = arrDomain.firstIndex(of: ".") ?? 0
+            arrDomain = Array(arrDomain[(dotIndex + 1)..<arrDomain.count])
         }
     }
-    
+
     return dict.compactMap { "\($0.value) \($0.key)" }
 }
 }
